aea1347bebc5b9fbe7abb946c6810dd4617a0d7f
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-04-06 12:34:36 +1000

    Create TableBlock.java

---
+++ src/main/java/io/github/tastac/dungeonsmod/common/block/TableBlock.java
@@ -0,0 +1,14 @@
+package io.github.tastac.dungeonsmod.common.block;
+
+import net.minecraft.block.Block;
+
+/**
+ * @author CoffeeCatRailway
+ * Created: 6/04/2020
+ */
+public class TableBlock extends Block {
+
+    public TableBlock(Properties properties) {
+        super(properties);
+    }
+}
