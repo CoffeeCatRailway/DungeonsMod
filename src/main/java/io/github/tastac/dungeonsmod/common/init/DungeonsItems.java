65432129f832b0f0ef8f1f102b85e8f480c2c7ae
Tastac <Keyganap@gmail.com>
2020-04-07 03:50:46 -0700

    Windhorn Artifact added w/ some functionality

--- src/main/java/io/github/tastac/dungeonsmod/common/init/DungeonsItems.java
+++ src/main/java/io/github/tastac/dungeonsmod/common/init/DungeonsItems.java
@@ -1,7 +1,9 @@
 package io.github.tastac.dungeonsmod.common.init;
 
 import io.github.tastac.dungeonsmod.DungeonsMod;
+import io.github.tastac.dungeonsmod.common.item.WindhornItem;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -9,5 +11,7 @@ public class DungeonsItems
 {
 
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, DungeonsMod.MOD_ID);
+
+    public static final RegistryObject<WindhornItem> WINDHORN = ITEMS.register("windhorn", () -> new WindhornItem(new Item.Properties()));
 
 }
