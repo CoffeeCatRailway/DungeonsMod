74e783d200a6a4cefac12608cb52cbe40166234e
Tastac <Keyganap@gmail.com>
2020-04-07 03:50:13 -0700

    Removed old Blocks

--- src/main/java/io/github/tastac/dungeonsmod/common/init/DungeonsBlocks.java
+++ src/main/java/io/github/tastac/dungeonsmod/common/init/DungeonsBlocks.java
@@ -1,7 +1,7 @@
 package io.github.tastac.dungeonsmod.common.init;
 
 import io.github.tastac.dungeonsmod.DungeonsMod;
-import io.github.tastac.dungeonsmod.common.block.PathBlock;
+import io.github.tastac.dungeonsmod.common.block.ModBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
@@ -17,13 +17,7 @@ public class DungeonsBlocks
 {
 
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, DungeonsMod.MOD_ID);
-
-    public static final RegistryObject<Block> CRATE = register("crate", () -> new Block(Block.Properties.from(Blocks.BARREL)));
-    public static final RegistryObject<Block> DIRT_PATH = register("dirt_path", () -> new PathBlock(Block.Properties.from(Blocks.DIRT)));
-    public static final RegistryObject<Block> ROCKY_DIRT_PATH = register("rocky_dirt_path", () -> new Block(Block.Properties.from(Blocks.COBBLESTONE)));
-    public static final RegistryObject<Block> STONE_BRICKS_TILE = register("stone_bricks_tile", () -> new Block(Block.Properties.from(Blocks.STONE_BRICKS)));
-    public static final RegistryObject<Block> DIRT_GRASSY_LESS = register("dirt_grassy_less", () -> new Block(Block.Properties.from(Blocks.DIRT)));
-
+    
     //Registry
 
     private static <T extends Block> RegistryObject<T> register(String name, Supplier<T> block)
