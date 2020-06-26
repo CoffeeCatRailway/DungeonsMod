a41e456038a8e049985d2c181899dabd9f5d4d6c
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-06-26 11:27:18 +1000

    Added set option to souls command

--- src/main/java/io/github/tastac/dungeonsmod/common/command/SoulsCommand.java
+++ src/main/java/io/github/tastac/dungeonsmod/common/command/SoulsCommand.java
@@ -19,27 +19,45 @@ import java.util.List;
  * Created: 23/06/2020
  */
 public class SoulsCommand {
+    
+    private static final int MAX_SOULS = 20;
+
+    private static final String ARG_TARGETS = "targets";
+    private static final String ARG_AMOUNT = "amount";
 
     @SuppressWarnings("ConstantConditions")
     public static void register(CommandDispatcher<CommandSource> dispatcher) {
         dispatcher.register(Commands.literal("souls").requires(src -> src.hasPermissionLevel(src.getServer().getOpPermissionLevel()))
-                .then(Commands.argument("targets", EntityArgument.players())
-                        .then(Commands.literal("add").then(Commands.argument("amount", IntegerArgumentType.integer(0, 10)).executes(ctx -> addOrRemoveSouls(ctx, false))))
-                        .then(Commands.literal("remove").then(Commands.argument("amount", IntegerArgumentType.integer(0, 10)).executes(ctx -> addOrRemoveSouls(ctx, true))))
+                .then(Commands.argument(ARG_TARGETS, EntityArgument.players())
+                        .then(Commands.literal("add").then(Commands.argument(ARG_AMOUNT, IntegerArgumentType.integer(0, MAX_SOULS)).executes(ctx -> addOrRemoveSouls(ctx, false))))
+                        .then(Commands.literal("remove").then(Commands.argument(ARG_AMOUNT, IntegerArgumentType.integer(0, MAX_SOULS)).executes(ctx -> addOrRemoveSouls(ctx, true))))
                         .then(Commands.literal("get").executes(ctx -> {
-                            ServerPlayerEntity player = ctx.getSource().asPlayer();
-                            ServerPlayerEntity targets = EntityArgument.getPlayer(ctx, "targets");
-                            targets.getCapability(SoulsCapibility.SOULS_CAP).ifPresent(handler -> {
-                                player.sendMessage(new StringTextComponent("Souls: " + handler.getSouls()));
-                            });
+                            List<ServerPlayerEntity> players = new ArrayList<>(EntityArgument.getPlayers(ctx, ARG_TARGETS));
+                            StringBuilder builder = new StringBuilder();
+                            players.forEach(player -> player.getCapability(SoulsCapibility.SOULS_CAP).ifPresent(handler -> {
+                                builder.append(player.getName().getString()).append("'s souls: ").append(handler.getSouls());
+                            }));
+                            ctx.getSource().asPlayer().sendMessage(new StringTextComponent(builder.toString()));
                             return 1;
-                        }))));
+                        }))
+                        .then(Commands.literal("set").then(Commands.argument(ARG_AMOUNT, IntegerArgumentType.integer(0, MAX_SOULS)).executes(ctx -> {
+                            int souls = IntegerArgumentType.getInteger(ctx, ARG_AMOUNT);
+                            List<ServerPlayerEntity> players = new ArrayList<>(EntityArgument.getPlayers(ctx, ARG_TARGETS));
+
+                            players.forEach(player -> player.getCapability(SoulsCapibility.SOULS_CAP).ifPresent(handler -> handler.setSouls(souls)));
+
+                            String playersMsg = players.size() + " players";
+                            if (players.size() == 1)
+                                playersMsg = players.get(0).getName().getString();
+                            ctx.getSource().asPlayer().sendMessage(new StringTextComponent("Set " + playersMsg + " souls(s) to " + souls));
+                            return 1;
+                        })))));
     }
 
     @SuppressWarnings("ConstantConditions")
     private static int addOrRemoveSouls(CommandContext<CommandSource> ctx, boolean remove) throws CommandSyntaxException {
-        List<ServerPlayerEntity> players = new ArrayList<>(EntityArgument.getPlayers(ctx, "targets"));
-        int amount = IntegerArgumentType.getInteger(ctx, "amount");
+        List<ServerPlayerEntity> players = new ArrayList<>(EntityArgument.getPlayers(ctx, ARG_TARGETS));
+        int amount = IntegerArgumentType.getInteger(ctx, ARG_AMOUNT);
         players.forEach(player -> player.getCapability(SoulsCapibility.SOULS_CAP).ifPresent(handler -> {
             if (remove)
                 handler.removeSouls(amount);
